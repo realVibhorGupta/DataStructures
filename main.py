8d844b140754b7e9a27d97879c83268dc5c22783
Vibhor Gupta <vibhor.88.gupta@gmail.com>
2021-10-11 11:33:52 +0000

    # What are the keywords in python

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
 
-def locate_card(cards, query):
-  pass
+# def locate_card(cards, query):
+#   print
+#   pass
 
+
+# a = 3 -- 2 -- 1
+# print(a)
+
+# What are the keywords in python
+
+import keyword
+
+print ("The list of keywords is : ")
+print (keyword.kwlist)
