bbbc0e9ef8a38f971ed2bede90bec88102c24f36
Vibhor Gupta <vibhor.88.gupta@gmail.com>
2021-10-11 11:46:33 +0000

    # How to check if a string is a keyword?

--- main.py
+++ main.py
@@ -13,3 +13,14 @@ import keyword
 
 print ("The list of keywords is : ")
 print (keyword.kwlist)
+
+# How to check if a string is a keyword?
+import keyword
+
+keys =  ['False', 'None', 'True', 'and', 'as', 'assert', 'async', 'await', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield','Vibhor']
+
+for counter in range(len(keys)):
+  if keyword.iskeyword(keys[counter]):
+      print(keys[counter] + " is a  python keyword")
+  else:
+      print(keys[counter] + " is not a python keyword")
